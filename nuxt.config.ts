898cde570a0d212108642fe8d0118864e2cd6254
oladetoungee <demilad1998@gmail.com>
2024-10-08 16:35:53 +0100

    dependencies cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: [
-    'vuetify/styles',
     '@mdi/font/css/materialdesignicons.css'
   ],
   build: {
